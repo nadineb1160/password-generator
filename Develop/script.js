88dfd67a609c2f09d7098b3678f3c66ddfd48c4c
nadineb <nadineb@umich.edu>
2020-03-19 17:12:27 -0700

    fixed generatePassword(), and .split, added copy button

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Copy button 
+var copyBtn = document.querySelector("#copy");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -7,105 +10,127 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+copyBtn.addEventListener("click", copyPassword);
 
-// Global Variables 
+
+// Copy password to clipboard
+function copyPassword() {
+  var passwordCopy = document.getElementById("password");
+  console.log(passwordCopy);
+  passwordCopy.select();
+
+  document.execCommand("copy");
+
+  alert("Password copied to clipboard");
+}
+
+
+// Global Variables Declared
 var lowerCase;
 var upperCase;
 var numeric;
 var specialChar;
-var length;
+var passwordLength;
 
 
 // Prompt Users for password criteria 
 function promptUser() {
 
+  // Prompt the user for password length
+  passwordLength = prompt("What length password would you like?")
+
+  // Continue to prompt user until requirement is met
+  // Length of at least 8 characters and no more than 128 characters
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert("Please try again");
+    passwordLength = prompt("Enter a new password length between 8-128");
+  }
+
+  // Confirm character types
   lowerCase = confirm("Would like lowercase characters?");
   upperCase = confirm("Would like uppercase characters?");
   numeric = confirm("Would like numeric characters?");
   specialChar = confirm("Would like specialChar characters?");
-  length = prompt("What length password would you like?")
 
-  if (length < 8 || length > 128) {
-    alert("Please try again");
-    length = prompt("Enter a new password length between 8-128");
-  }
 
-  // if no character type selected
+  // If no character type selected
   if (lowerCase === false && upperCase === false && 
     numeric === false && specialChar === false) {
+      // Continue to prompt user until at least one type valid
       promptUser();
     }
 
 }
 
-promptUser();
 
 // Alphabetic and numeric strings
 var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var specialCharLetters = "~!@#$%^&*?_<>+{}";
 
-// Split string and 
-var lowerCaseArray = lowerCaseLetters.split();
-var upperCaseArray = upperCaseLetters.split();
+
+// Arrays with different charater types
+var lowerCaseArray = lowerCaseLetters.split("");
+var upperCaseArray = upperCaseLetters.split("");
 var numericArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialCharArray =  specialCharLetters.split();
+var specialCharArray =  specialCharLetters.split("");
 
 
-
-// Valid character Array
+// Valid character Array - nested [["upperArray"],["lowerArray"]]
 var validChar = [];
 
-// Check prompts
+// Check prompts to see which types are valid
 function checkPrompts() {
+
   // Check if lowercase valid
   if(lowerCase) {
     validChar.push(lowerCaseArray);
   }
   // Check if uppercase valid
-  if(upperCaseArray) {
+  if(upperCase) {
     validChar.push(upperCaseArray);
   }
   // Check if numeric valid
-  if(numericArray) {
+  if(numeric) {
     validChar.push(numericArray);
   }
   // Check if special char valid
-  if(specialCharArray) {
+  if(specialChar) {
     validChar.push(specialCharArray);
   }
 }
 
-// Generates the type of digit
+// Generates the type of digit - "lowerCase" "numeric"
 function generateRandomType() {
   // generate a number between 0 and validChar length
-  return Math.floor(Math.random() * validChar.length)
+  return Math.floor(Math.random() * validChar.length);
 }
 
-// Generates the char in type array
+// Generates the char in type array - "k" 9
 function generateRandomChar(typeIdx) {
   // generate a number between 0 and typeIdx array length
-  return Math.floor(Math.random() * validChar[typeIdx].length)
+  return Math.floor(Math.random() * validChar[typeIdx].length);
 }
 
-// Make password
+// Make password using valid characters in validChar
 function makePassword() {
 
   // Password Array
-  var password = []
+  var password = [];
 
-  // for each character in the password
-  for (var i = 0; i < length; i++) {
+  // For each character in the password
+  for (var i = 0; i < passwordLength; i++) {
 
-    // randomly generate number between 0 and validChar array
+    // Randomly generate number between 0 and validChar array
+    // This indicates which type we will use
     var typeIdx = generateRandomType();
 
-    // randomly generate number between 0 and typeIdx array  array
+    // Randomly generate number between 0 and typeIdx array 
+    // This indicates which charater to pick in typeIdx array
     var charIdx = generateRandomChar(typeIdx);
 
     // Charater for position i
@@ -117,9 +142,24 @@ function makePassword() {
   }
 
   // Join password letters together
-  return password.join();
+  return password.join("");
 }
 
+// Generate the password 
+function generatePassword() {
+  // Set variables for user options
+  promptUser();
+
+  // Check which prompts are valid, pushes valid arrays to validChar
+  checkPrompts();
+
+  // Make password based on availablle characters in validChar
+  var pass = makePassword();
+
+  // Return Password
+  // console.log for now
+  return pass;
+}
 
 
 // THEN a password is generated that matches the selected criteria
