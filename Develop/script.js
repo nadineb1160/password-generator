8e19ee38bad36e3bc2bde778723d17f9c7b03c32
nadineb <nadineb@umich.edu>
2020-03-20 20:58:26 -0700

    grammar check and addeed comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,71 +1,6 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-// Copy button - added to assignment code
-var copyBtn = document.querySelector("#copy");
-
-
-// Write password to the #password input
-function writePassword() {
-  // Create new variables each time a password is made
-  var lowerCase = false;
-  var upperCase = false;
-  var numeric = false;
-  var specialChar = false;
-  var passwordLength = 0;
-
-  // Generate password string
-  var password = generatePassword();
-  // Locate password text in document
-  var passwordText = document.querySelector("#password");
-  // Assign password text the password generated
-  passwordText.value = password;
-}
-
-// Copy password to clipboard
-function copyPassword() {
-  // Get password element to copy
-  var passwordCopy = document.getElementById("password");
-  // Select text to have copied
-  passwordCopy.select();
-  // Execuate copy command
-  document.execCommand("copy");
-  // Alert user that password is copied
-  alert("Password copied to clipboard");
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-// Add event listener to copy button
-copyBtn.addEventListener("click", copyPassword);
-
-
-// Prompt user for password criteria 
-function promptUser() {
-
-  // Prompt the user for password length
-  passwordLength = prompt("What length password would you like?");
-
-  // Continue to prompt user until requirement is met
-  // Length of at least 8 characters and no more than 128 characters
-  while (passwordLength < 8 || passwordLength > 128) {
-    alert("Please try again");
-    passwordLength = prompt("Enter a new password length between 8-128");
-  }
-
-  // Confirm character types
-  lowerCase = confirm("Would like lowercase characters?");
-  upperCase = confirm("Would like uppercase characters?");
-  numeric = confirm("Would like numeric characters?");
-  specialChar = confirm("Would like specialChar characters?");
-
-  // If no character type selected
-  if (lowerCase === false && upperCase === false && 
-    numeric === false && specialChar === false) {
-      // Continue to prompt user until at least one type valid
-      promptUser();
-    }
-}
-
+// *****************
+// *** VARIABLES ***
+// *****************
 
 // Alphabetic and numeric strings
 var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
@@ -79,8 +14,100 @@ var numericArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharArray =  specialCharLetters.split("");
 
 // Valid character Array - nested [["upperArray"],["lowerArray"]]
+// Contains ONLY valid character arrays user requests
 var validChar;
 
+// Generate button 
+var generateBtn = document.querySelector("#generate");
+// Copy button 
+var copyBtn = document.querySelector("#copy");
+
+// *****************
+// ***** WRITE *****
+// *****************
+
+// Write password to the #password input
+function writePassword() {
+
+  // Initialize new variables each time a password is made
+  var lowerCase = false;
+  var upperCase = false;
+  var numeric = false;
+  var specialChar = false;
+  var passwordLength = 0;
+
+  // Generate password string
+  var password = generatePassword();
+  // Locate password text in document
+  var passwordText = document.querySelector("#password");
+  // Assign password generated to the password text
+  passwordText.value = password;
+}
+
+// *****************
+// ***** COPY ******
+// *****************
+
+// Copy password to clipboard
+function copyPassword() {
+  // Get password element to copy
+  var passwordCopy = document.getElementById("password");
+  // Select text to have copied
+  passwordCopy.select();
+  // Execuate copy command
+  document.execCommand("copy");
+  // Alert user that password is copied
+  alert("Password copied to clipboard");
+}
+
+// *****************
+// * EVENTLISTENER *
+// *****************
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+// Add event listener to copy button
+copyBtn.addEventListener("click", copyPassword);
+
+
+// *****************
+// **** PROMPT *****
+// *****************
+
+// Prompt user for password criteria 
+function promptUser() {
+
+  // Prompt the user for password length
+  passwordLength = prompt("What length password would you like?");
+
+  // Continue to prompt user until requirement is met
+  // Length of at least 8 characters and no more than 128 characters
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert("Please try again");
+    // Give user further specification
+    passwordLength = prompt("Enter a new password length between 8-128");
+  }
+
+  // Confirm character types
+  lowerCase = confirm("Would like lowercase characters?");
+  upperCase = confirm("Would like uppercase characters?");
+  numeric = confirm("Would like numeric characters?");
+  specialChar = confirm("Would like specialChar characters?");
+
+  // If no character type selected
+  if (lowerCase === false && upperCase === false && 
+    numeric === false && specialChar === false) {
+      // alert user to select character type
+      alert("Try again: You didn't select a character type!");
+      // Continue to prompt user until at least one type valid
+      promptUser();
+    }
+}
+
+
+// *****************
+// ***** CHECK *****
+// *****************
 
 // Check prompts to see which types are valid
 function checkPrompts() {
@@ -106,22 +133,35 @@ function checkPrompts() {
   }
 }
 
+// *****************
+// ***** TYPEID ****
+// *****************
 
 // Generates the type of digit - "lowerCase"
 function generateRandomType() {
+
   // Generate a number between 0 and validChar length
   return Math.floor(Math.random() * validChar.length);
 }
 
 
+// *****************
+// ***** CHARID ****
+// *****************
+
 // Generates the char in type array - "k"
 function generateRandomChar(typeIdx) {
+
   // Generate a number between 0 and typeIdx array length
   return Math.floor(Math.random() * validChar[typeIdx].length);
 }
 
 
-// Make password using valid characters in validChar
+// *****************
+// ***** MAKE ******
+// *****************
+
+// Make password using valid characters in validChar array
 function makePassword() {
 
   // Password Array
@@ -141,17 +181,22 @@ function makePassword() {
     // Charater for position i
     var randomChar = validChar[typeIdx][charIdx];
 
-    // Add element to password
+    // Add element to password array
     password.push(randomChar);
 
   }
-  // Join password letters together
+  // Join password array into string
   return password.join("");
 }
 
 
+// *****************
+// **** GENERATE ***
+// *****************
+
 // Generate the password 
 function generatePassword() {
+
   // Set variables for user options
   promptUser();
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# JavaScript: Password Generator
+# Password Generator
 
-I created an application that generates a random password based on user-selected criteria.
+This is an application that generates a random password based on user-selected criteria.
 
 The app runs in the browser and features dynamically updated HTML and CSS powered by Javascript.
 
@@ -16,15 +16,16 @@ The app runs in the browser and features dynamically updated HTML and CSS powere
 - If prompted for character types
 -> Then I can choose lowercase, uppercase, numeric, and/or special characters
 
-- If all prompts are anwered and at least one character type is selected
+- If all prompts are answered and at least one character type is selected
 -> Then a password is generated and displayed
 
 - If I click the "copy" button
 -> Then the password generated is copied to the clipboard
 
 ```
+# 
 
-The following image demonstrates the application functionality:
+##  The following image demonstrates the application functionality:
 
 ![password generator demo](./Assets/03-hw-demo.png)
 
@@ -32,11 +33,21 @@ The following image demonstrates the application functionality:
 
 ## How it generates a password:
 
-There is a nested array that holds character-type arrays (ex. numeric = [0,1,2,..]) based on what the user wants in the password.
+The password is generated using  a nested array. 
+The array validChar holds character-type arrays:
 
-In generateRandomType(), I randomly generate a number that represents one of these valid arrays. Then in generateRandomChar(), I randomly generate a number for the index of that type. 
+```
+validChar = [
+    ["a","b","c",...], 
+    ["A","B","C",...], 
+    [0,1,2,..], 
+    ["?","@","!",...]]
+```
+ 
+The validChar array will only have the arrays of characters that the user has requested. Two functions are used to find the character index at random:
 
-This is done for each letter generated in the password such that all options the user requested are available but not necessaily guaranteed.
+- generateRandomType() - a number is randomly generated that represents the index of one of these valid arrays (ex. index between 0-3).
+- generateRandomChar() - a number is randomly generated for the index of that type (ex. index between 0-25). 
 
 ```
 // Generates the type of digit - "lowerCase"
@@ -53,11 +64,14 @@ function generateRandomChar(typeIdx) {
 }
 ```
 
+This is done for each letter generated in the password such that all character type options the user requested are available but not necessaily guaranteed.
+
+# 
 
 ## Technologies Used
 - HTML - used to create elements on the DOM
 - CSS - styles html elements on page
-- Javascript - adds dynamic features
+- Javascript - dynamically creates random password
 - Git - version control system to track changes to source code
 - GitHub - hosts repository that can be deployed to GitHub Pages
 
